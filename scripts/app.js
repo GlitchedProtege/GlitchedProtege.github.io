26c8b5176aa6698e10570fdf67f959431e13af81
Liam <ljcallaghan64@gmail.com>
2020-11-27 00:25:38 +0000

    contact styling

--- scripts/app.js
+++ scripts/app.js
@@ -30,6 +30,57 @@ function init() {
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     )
   }
+
+  let carouselNum = 0
+
+  const nextButton = document.querySelector('.next')
+  const prevButton = document.querySelector('.prev')
+
+  const project1 = document.querySelector('.one')
+  const project2 = document.querySelector('.two')
+  const project3 = document.querySelector('.three')
+  const project4 = document.querySelector('.four')
+  const projects = document.querySelectorAll('.project')
+
+  function changeSlide() {
+    projects.forEach(element => {
+      element.classList.remove('selected')
+    })
+    if (carouselNum === 0) {
+      project1.classList.add('selected')
+    } else if (carouselNum === 1) {
+      project2.classList.add('selected')
+    } else if (carouselNum === 2) {
+      project3.classList.add('selected')
+    } else {
+      project4.classList.add('selected')
+    }
+  }
+
+  changeSlide()
+
+  function nextSlide() {
+    if (carouselNum < 3) {
+      carouselNum += 1
+    } else {
+      carouselNum = 0
+    }
+    // console.log(carouselNum)
+    changeSlide()
+  }
+
+  function lastSlide() {
+    if (carouselNum > 0) {
+      carouselNum -= 1
+    } else {
+      carouselNum = 3
+    }
+    // console.log(carouselNum)
+    changeSlide()
+  }
+
+  nextButton.addEventListener('click', nextSlide)
+  prevButton.addEventListener('click', lastSlide)
 }
 
 window.addEventListener('DOMContentLoaded', init)
